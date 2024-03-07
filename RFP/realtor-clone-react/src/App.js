6edcf0030dc5285c55aa9c3c8891757d5cf694f8
Kraken57 <kasaad1609@gmail.com>
2024-03-07 23:04:26 +0530

    Update template paths and Tailwind directives

--- RFP/realtor-clone-react/src/App.js
+++ RFP/realtor-clone-react/src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <>
-      <h1>Hello World!</h1>
+      <h1 className="text-2xl bg-red-400">Hello World!</h1>
     </>
   );
 }
