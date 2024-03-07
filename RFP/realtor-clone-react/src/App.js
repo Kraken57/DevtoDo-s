25aa5cb91abae8ee4c0175760f1d0dc916ce402f
Kraken57 <kasaad1609@gmail.com>
2024-03-07 23:12:42 +0530

    code clear

--- RFP/realtor-clone-react/src/App.js
+++ RFP/realtor-clone-react/src/App.js
@@ -1,10 +1,9 @@
-import logo from "./logo.svg";
-import "./App.css";
+
 
 function App() {
   return (
     <>
-      <h1 className="text-2xl bg-red-400">Hello World!</h1>
+      <h1>Hello World!</h1>
     </>
   );
 }
