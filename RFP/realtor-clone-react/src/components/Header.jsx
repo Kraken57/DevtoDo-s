9d1adc87cb020a1f2849dc22f196e560c773db55
Kraken57 <kasaad1609@gmail.com>
2024-03-08 18:41:33 +0530

    Added useLocation logic and header css to header

--- RFP/realtor-clone-react/src/components/Header.jsx
+++ RFP/realtor-clone-react/src/components/Header.jsx
@@ -1,12 +1,25 @@
-import React from "react";
+import { useLocation } from "react-router-dom";
+{
+  /**useLocation is used to get the router address and make logic accordingly  */
+}
 
 export default function Header() {
+  const location = useLocation();
+  {
+    /*function to check what is the current route and match it and make the changes to the header  */
+  }
+  function pathMatchRoute(route) {
+    if (route === location.pathname) {
+      return true;
+    }
+  }
   return (
     <div className="bg-white border-b shadow-sm sticky top-0 z-50">
       {/* here we are making the border of the header 
        "sticky" is used here to keep the header on top visible even if you scroll up and down
       and "z-50" is used to keep the header always on the top (z-1 means up and z-0 means bottom) */}
-      <header className="flex justify-between items-center px-3 max-w-6xl mx-auto">{/*"max-w-6xl mx-auto" is used to keep the padding in the maximize screen */}
+      <header className="flex justify-between items-center px-3 max-w-6xl mx-auto">
+        {/*"max-w-6xl mx-auto" is used to keep the padding in the maximize screen */}
         <div>
           <img
             src="https://static.rdc.moveaws.com/images/logos/rdc-logo-default.svg"
@@ -16,9 +29,28 @@ export default function Header() {
         </div>
         <div>
           <ul className="flex space-x-10">
-            <li>Home</li>
-            <li>Offers</li>
-            <li>Sign In</li>
+            <li
+              className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
+                pathMatchRoute("/") && "text-black border-b-red-500"
+              }`}
+            >
+              Home
+            </li>
+            <li
+              className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
+                pathMatchRoute("/offers") && "text-black border-b-red-500"
+              }`}
+            >
+              Offers
+            </li>
+            <li
+              className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
+                pathMatchRoute("/sign-incursor-pointer ") &&
+                "text-black border-b-red-500"
+              }`}
+            >
+              Sign In
+            </li>
           </ul>
         </div>
       </header>
