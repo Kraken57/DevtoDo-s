e605fc637918ff20094bc0166dc46bfe9f102a2f
Kraken57 <kasaad1609@gmail.com>
2024-03-08 18:18:11 +0530

    Added header border and sticky feature to header

--- RFP/realtor-clone-react/src/components/Header.jsx
+++ RFP/realtor-clone-react/src/components/Header.jsx
@@ -2,8 +2,11 @@ import React from "react";
 
 export default function Header() {
   return (
-    <div>
-      <header className="flex justify-between items-center px-3 max-w-6xl mx-auto">
+    <div className="bg-white border-b shadow-sm sticky top-0 z-50">
+      {/* here we are making the border of the header 
+       "sticky" is used here to keep the header on top visible even if you scroll up and down
+      and "z-50" is used to keep the header always on the top (z-1 means up and z-0 means bottom) */}
+      <header className="flex justify-between items-center px-3 max-w-6xl mx-auto">{/*"max-w-6xl mx-auto" is used to keep the padding in the maximize screen */}
         <div>
           <img
             src="https://static.rdc.moveaws.com/images/logos/rdc-logo-default.svg"
