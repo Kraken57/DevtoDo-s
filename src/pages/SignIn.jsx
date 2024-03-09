a230266f9eaaae29f0a3b1442d3c42a080ff5409
Kraken57 <kasaad1609@gmail.com>
2024-03-09 19:18:52 +0530

    build basic sign in interface and added onChange function etc

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,5 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function SignIn() {
-  return <div>Sign In</div>;
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const { email, password } = formData;
+  function onChange(e) {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  }
+  {
+    /*This function updates the form data state when input fields change.
+    It takes an event (e) as input, which contains information about the input field that triggered the change.
+    The spread operator (...) makes a copy of the previous state object (prevState).
+    Computed property names ([e.target.id]: e.target.value) dynamically update the specific property in the copied state object.
+    Finally, the updated state object is passed to setFormData to update the form data state.*/
+  }
+  return (
+    <section>
+      <h1 className="text-3xl text-center mt-6 font-bold">Sign In</h1>
+      <div className="flex justify-center flex-wrap items-center px-6 py-12 max-w-6xl mx-auto">
+        <div className="md:w-[67%] lg:w-[50%] mb-12 md:mb-6">
+          <img
+            src="https://images.unsplash.com/photo-1560518883-ce09059eeffa?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fHJlYWwlMjBlc3RhdGV8ZW58MHx8MHx8fDA%3D"
+            alt="logo"
+            className="w-full rounded-2xl"
+          />
+        </div>
+        <div className="w-full md:w-[67%] lg:w-[40%] lg:ml-20">
+          <form>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={onChange}
+              placeholder="Email Address"
+              className=""
+            />
+          </form>
+        </div>
+      </div>
+    </section>
+  );
 }
