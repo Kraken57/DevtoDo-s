e2378423d7214d90628f5aa798e81f568ac14187
Kraken57 <kasaad1609@gmail.com>
2024-03-13 11:21:48 +0530

    Ui of forgot passcode page

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -1,5 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import OAuth from "../components/OAuth";
 
 export default function ForgotPassword() {
-  return <div>Forgot Password</div>;
+  const [email, setEmail] = useState("");
+
+  function onChange(e) {
+    setEmail(e.target.value);
+  }
+
+  /*This function updates the form data state when input fields change.
+    It takes an event (e) as input, which contains information about the input field that triggered the change.
+    The spread operator (...) makes a copy of the previous state object (prevState).
+    Computed property names ([e.target.id]: e.target.value) dynamically update the specific property in the copied state object.
+    Finally, the updated state object is passed to setFormData to update the form data state.*/
+
+  return (
+    <section>
+      <h1 className="text-3xl text-center mt-6 font-bold">Forgot Password</h1>
+      <div className="flex justify-center flex-wrap items-center px-6 py-12 max-w-6xl mx-auto">
+        <div className="md:w-[67%] lg:w-[50%] mb-12 md:mb-6">
+          <img
+            src="https://images.unsplash.com/photo-1560518883-ce09059eeffa?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fHJlYWwlMjBlc3RhdGV8ZW58MHx8MHx8fDA%3D"
+            alt="logo"
+            className="w-full rounded-2xl"
+          />
+        </div>
+        <div className="w-full md:w-[67%] lg:w-[40%] lg:ml-20">
+          <form>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={onChange}
+              placeholder="Email Address"
+              className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-500 rounded transition ease-in-out"
+            />
+            <div className="flex justify-between whitespace-nowrap text-sm sm:text-lg">
+              <p className="mb-6">
+                Don't have a account?
+                <Link
+                  to="/sign-up"
+                  className="text-red-600 hover:text-red-700 transition duration-200 ease-in-out ml-1"
+                >
+                  Register
+                </Link>
+              </p>
+              <p>
+                <Link
+                  to="/sign-in"
+                  className="text-blue-600 hover:text-blue-800 transition duration-200 ease-in-out"
+                >
+                  Sign in instead
+                </Link>
+              </p>
+            </div>
+            <button
+              type="submit"
+              className="w-full bg-blue-600 text-white px-7 py-3 text-sm font-medium uppercase rounded shadow-md hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800"
+            >
+              Send Reset Email
+            </button>
+            <div className="flex items-center my-4 before:border-b before:flex-1 before:border-gray-300 after:border-b  after:flex-1 after:border-gray-300">
+              <p className="text-center font-semibold mx-4">OR</p>
+            </div>
+            <OAuth />
+          </form>
+        </div>
+      </div>
+    </section>
+  );
 }
